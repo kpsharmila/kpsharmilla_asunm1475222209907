9375492e77c1ead36ff26340a4de4a2064b136bb
kpsharmila <145069008+kpsharmila@users.noreply.github.com>
2023-09-27 03:10:21 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,69 +1,21 @@
-class player
-{
-String name;
-int age;
-player(String n,int a)
-{ name=n; age=a; }
-void show()
-{
-System.out.println("\n");
-System.out.println("Player name : "+name);
-System.out.println("Age : "+age);
-}
-}
-class criket_player extends player
-{
-String type;
-criket_player(String n,String t,int a)
-{
-super(n,a);
-type=t;
-}
-public void show()
-{
-super.show();
-System.out.println("Player type : "+type);
-}
-}
-class football_player extends player
-{
-String type;
-football_player(String n,String t,int a)
-{
-super(n,a);
-type=t;
-}
-public void show()
-{
-super.show();
-System.out.println("Player type : "+type);
-}
-}
-class hockey_player extends player
-{
-String type;
-hockey_player(String n,String t,int a)
-{
-super(n,a);
-type=t;
-}
-public void show()
-{
-super.show();
-System.out.println("Player type : "+type);
-}
-}
-//--------- main -----------
-class s04_02
-{
-public static void main(String args[])
-{
-criket_player c=new criket_player("Ameer","criket",25);
-football_player f=new football_player("arun","foot ball",25);
-hockey_player h=new hockey_player("Ram","hockey",25);
-c.show();
-f.show();
-h.show();
-}
-}
 
+def search(arr, N, x):
+  
+    for i in range(0, N):
+        if (arr[i] == x):
+            return i
+    return -1
+  
+  
+# Driver Code
+if __name__ == "__main__":
+    arr = [2, 3, 4, 10, 40]
+    x = 10
+    N = len(arr)
+  
+    # Function call
+    result = search(arr, N, x)
+    if(result == -1):
+        print("Element is not present in array")
+    else:
+        print("Element is present at index", result)
